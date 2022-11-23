88b0c0db4a43ad5b68be877cf6b8c89199822ba2
Aimee <aimeeray3@gmail.com>
2022-11-23 16:22:48 -0700

    deployment lab

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+app.use(express.static(`${__dirname}/public`))
+app.listen(4000, () => console.log(`server running on 4000`))
