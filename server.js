2db1ea830f847b9fc9f95c7b4006fadecbd797d3
Aimee <aimeeray3@gmail.com>
2022-11-23 17:30:29 -0700

    testing

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+app.use(express.json())
+app.use(cors())
 app.use(express.static(`${__dirname}/public`))
 app.listen(4000, () => console.log(`server running on 4000`))
